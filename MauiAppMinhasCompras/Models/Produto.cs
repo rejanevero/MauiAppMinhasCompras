fb7387371efc48fff96c401f6717453845771333
Rejane Verônica <rejanevero@gmail.com>
2025-02-22 17:28:15 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public double Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
