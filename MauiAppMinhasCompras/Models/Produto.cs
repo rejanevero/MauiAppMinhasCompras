d805ffff6c2d86dda967ac17bedfc2266426ccfc
Rejane Verônica <rejanevero@gmail.com>
2025-03-07 14:52:13 -0300

    Modificações para a  agenda 03.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,11 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+       
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public double Descricao { get; set; }
+        public string Descricao { get; set; }
+
         public double Quantidade { get; set; }
         public double Preco { get; set; }
 
